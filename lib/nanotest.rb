52da050fca241a3824a5f3a3cc2e2488c88710bd
mynyml <mynyml@gmail.com>
2009-12-25 11:29:19 -0500

    Handle filenames with colons. Fixes #1
    Thanks to ukabu for the report and initial patch.

--- lib/nanotest.rb
+++ lib/nanotest.rb
@@ -3,17 +3,16 @@ module Nanotest
 
   @@failures, @@errors, @@dots = [], [], []
 
-  def assert(msg=nil, file=nil, line=nil, &block)
-    file ||= caller.first.split(':')[0]
-    line ||= caller.first.split(':')[1]
+  def assert(msg=nil, file=nil, line=nil, stack=caller, &block)
+    f,l = stack.first.match(/(.*):(\d+)/)[1..2]
     unless block.call
-      @@failures << "(%s:%0.3d) %s" % [file, line, msg || "assertion failed"]
+      @@failures << "(%s:%0.3d) %s" % [file || f, line || l, msg || "assertion failed"]
       @@dots << 'F'
     else
       @@dots << '.'
     end
   rescue Exception => e
-    @@errors << "(%s:%0.3d) %s" % [file, line, msg || "assertion raised error: #{e.class}: #{e.message}"]
+    @@errors << "(%s:%0.3d) %s" % [file || f, line || l, msg || "assertion raised error: #{e.class}: #{e.message}"]
     @@dots << 'E'  
   end
 
